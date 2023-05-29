f87c2f3782bb04514d7811261bafc6e4eb340432
eduardoarellano <eduardo@glouu.com>
2023-05-28 23:50:04 -0600

    Set up basic API and web application structure

---
+++ api/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
+
+app.get('/api/users', (req, res) => {
+    res.json([
+        { id: 1, name: 'Usuario Test' }
+    ]);
+});
